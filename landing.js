dc56a1b65b9b1d5b072e708b27bd3b59ef468b40
Jeremy Lee <germyjermy@yahoo.com>
2015-04-25 16:30:07 -0400

    fixed mobile menu, changed language specification

--- landing.js
+++ landing.js
@@ -15,35 +15,35 @@
  	if(document.cookie.indexOf("lang") >= 0){
  		$('#splash').hide();
  		$('#main').show();
-        $("#lang").text("Language: " + getCookie("lang"));
+        $("#lang").text("Choose Language: " + getCookie("lang"));
  	}
 
  	$('#english').click(function(){
         document.cookie="lang=english; expires=Thu, 18 Dec 3000 12:00:00 UTC";        
  		$('#splash').hide();
  		$('#main').show();
-        $("#lang").text("Language: " + getCookie("lang"));
+        $("#lang").text("Choose Language: " + getCookie("lang"));
  	});
     
  	$('#spanish').click(function(){
  		$('#splash').hide();
  		$('#main').show();
         document.cookie="lang=spanish; expires=Thu, 18 Dec 3000 12:00:00 UTC";
-        $("#lang").text("Language: " + getCookie("lang"));
+        $("#lang").text("Choose Language: " + getCookie("lang"));
  	});
 
     $('#french').click(function(){
  		$('#splash').hide();
  		$('#main').show();
         document.cookie="lang=french; expires=Thu, 18 Dec 3000 12:00:00 UTC";
-        $("#lang").text("Language: " + getCookie("lang"));
+        $("#lang").text("Choose Language: " + getCookie("lang"));
  	});
     
     $('#chinese').click(function(){
  		$('#splash').hide();
  		$('#main').show();
         document.cookie="lang=chinese; expires=Thu, 18 Dec 3000 12:00:00 UTC";
-        $("#lang").text("Language: " + getCookie("lang"));
+        $("#lang").text("Choose Language: " + getCookie("lang"));
  	});    
     
     $('#lang').click(function(){
