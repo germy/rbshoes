884aa4574f491f6f5543824d8c8065da3ad81f3f
Jeremy Lee <germyjermy@yahoo.com>
2015-04-25 15:00:07 -0400

    attempted cookie fix

--- landing.js
+++ landing.js
@@ -1,37 +1,49 @@
+
+ 
+ 
  $(document).ready(function(){
+     
+  // Original JavaScript code by Chirp Internet: www.chirp.com.au
+  // Please acknowledge use of this code by including this header.
+  
+    function getCookie(name){
+        var re = new RegExp(name + "=([^;]+)");
+        var value = re.exec(document.cookie);
+        return (value != null) ? unescape(value[1]) : null;
+    }      
 
  	if(document.cookie.indexOf("lang") >= 0){
  		$('#splash').hide();
  		$('#main').show();
-        $("#lang").text("Language: " + document.lang);
+        $("#lang").text("Language: " + getCookie(lang));
  	}
 
  	$('#english').click(function(){
         document.cookie="lang=english; expires=Thu, 18 Dec 3000 12:00:00 UTC";        
  		$('#splash').hide();
  		$('#main').show();
-        $("#lang").text("Language: " + document.lang);
+        $("#lang").text("Language: " + getCookie(lang));
  	});
     
  	$('#spanish').click(function(){
  		$('#splash').hide();
  		$('#main').show();
         document.cookie="lang=spanish; expires=Thu, 18 Dec 3000 12:00:00 UTC";
-        $("#lang").text("Language: " + document.lang);
+        $("#lang").text("Language: " + getCookie(lang));
  	});
 
     $('#french').click(function(){
  		$('#splash').hide();
  		$('#main').show();
         document.cookie="lang=french; expires=Thu, 18 Dec 3000 12:00:00 UTC";
-        $("#lang").text("Language: " + document.lang);
+        $("#lang").text("Language: " + getCookie(lang));
  	});
     
     $('#chinese').click(function(){
  		$('#splash').hide();
  		$('#main').show();
         document.cookie="lang=chinese; expires=Thu, 18 Dec 3000 12:00:00 UTC";
-        $("#lang").text("Language: " + document.lang);
+        $("#lang").text("Language: " + getCookie(lang));
  	});    
     
     $('#lang').click(function(){
